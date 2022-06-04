52ebe9ed38bcc8391a2e1f98e48b59ad2f9b75a5
unknown <mensahjunior13@gmil.com>
2022-06-04 22:20:03 +0000

    init finish

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 
 
-const url = 'https://motherless.com/GID5AF19B?page=5';
-var img_url = 'https://cdn5-images.motherlessmedia.com/images/'
+const url = '';
+var img_url = ''
 
 
 async function download(uri, filename, callback) {
     request.head(url, function (err, res, body) {
-        // console.log('content-type:', res.headers['content-type']);
-        // console.log('content-length:', res.headers['content-length']);
         request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
 };
@@ -33,45 +31,8 @@ rp(url)
                 }, error => {
                     console.log('error download');
                 });
-
         }
-
-        // await uri_list.map(async e => {
-        //     console.log('attr', e.attribs);
-        // })
-        // && x.attribs.src.includes('-small')
-        // uri_list.filter(x => x.attribs)
-        //         .filter(y => y.attribs.hasOwnProperty('src') )
-        //         .map(uri => {
-
-        //     // let uri = "https://cdn5-thumbs.motherlessmedia.com/thumbs/6B84F76-small.jpg"
-        //     
-        // })
-
-
-        // $('.desktop-thumb a img')[3]//.attribs.alt
-        // const wikiUrls = $($('.desktop-thumb.medium a')[0] +' img')[1].attribs.src
-
-        // for (let i = 0; i < 45; i++) {
-        // wikiUrls.push($('big > a', html)[i].attribs.href);
-        // }
-        // console.log(wikiUrls);
-        // console.log($.html());
     })
     .catch(function (err) {
-        //handle error
         console.log('error', err);
     });
-
-
-
-
-// download('https://cdn5-images.motherlessmedia.com/images/F8F520C.jpg', 'F8F520C.jpg', () => {
-//     console.log('done');
-// }, error => {
-//     console.log('error download');
-// });
-
-// // 'https://cdn5-thumbs.motherlessmedia.com/thumbs/F008621-zoom.jpg'
-// // 'https://cdn5-images.motherlessmedia.com/images/F008621.jpg'
-// // 'https://cdn5-thumbs.motherlessmedia.com/thumbs/F8F520C-zoom.jpg'
